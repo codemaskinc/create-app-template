cc9b8bb8e8877d06dda27849404e43dcce101847
Hubert Bieszczad <brentlokk@gmail.com>
2023-10-25 15:13:27 +0200

    chore: add nestjs rest api template

--- src/utils/cloneTemplate.ts
+++ src/utils/cloneTemplate.ts
@@ -4,11 +4,13 @@ import { Template } from '../types/index.js'
 export const cloneTemplate = async (template: Template, path: string) => {
     const repoLink = (() => {
         switch (template) {
+        // TODO: Add another templates
         case Template.React:
             return 'https://github.com/codemaskinc/react-template'
         case Template.ReactNative:
         case Template.Astro:
         case Template.NestJSRestApi:
+            return 'https://github.com/codemaskinc/api-starter'
         case Template.NestJSGraphQL:
         default:
             return ''
